e8eae600adefdf11f1755692ed35cdf8d282dacf
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-08-08 19:10:18 +0300

    HEHE BOI

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  const header = document.getElementById('header');
+  const main = document.getElementById('main');
+  const body = document.getElementById('page__body');
+
+  if (window.location.hash === '#side-menu') {
+    header.classList.add('header--blur');
+    main.classList.add('header--blur');
+    body.classList.add('page__body--with-menu');
+  } else {
+    header.classList.remove('header--blur');
+    main.classList.remove('header--blur');
+    body.classList.remove('page__body--with-menu');
+  }
+});
